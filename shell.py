fdce7628819067d6adabc426086a9a501b3f16c9
ThatCosmicStorm <98968812+ThatCosmicStorm@users.noreply.github.com>
2025-06-20 21:02:54 -0600

    Add more info to shell.py

--- shell.py
+++ shell.py
@@ -1,6 +1,13 @@
-import logic
+import logic, os
+
+os.system("cls" if os.name == "nt" else "clear")
+
+version = "InDev"
+version_date = "June 20, 2025"
+
+print(f"ReadAbl {version} ({version_date})")
 
 running = True
 while running:
-    program = input("ReadAbl > ")
+    program = input(">>> ")
     logic.lexer(program)
