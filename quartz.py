837565de0a44c9d6e2135382d38de2d313cab9dd
ThatCosmicStorm <astronomerr2@gmail.com>
2025-09-04 23:25:25 -0400

    Update quartz.py
    It's set up now, not meaning it has all of the features, to work like an interpreter in the terminal, but you can also use it to open `.qrtz` files externally, or any plain text file for that matter. I also made it easier to use for debugging purposes.

--- quartz.py
+++ quartz.py
@@ -3,17 +3,40 @@ Currently only displays the output of the lexer.
 """
 
 import os
-from pprint import pprint
+import sys
 from lexer import Lexer
 os.system("cls" if os.name == "nt" else "clear")
 
 VERSION = "InDev"
 VERSION_DATE = "September 1, 2025"
 
-print(f"Quartz {VERSION} ({VERSION_DATE})")
 
-RUNNING = True
-while RUNNING:
-    program = input(">>> ")
+def quartz(program):
     tokens = Lexer(program).lexer()
-    pprint(tokens)
+    for token in tokens:
+        print(token)
+
+
+def main(filename=None, debugging=False):
+    if len(sys.argv) == 2 or debugging:
+        if filename is not None:
+            file = filename
+        else:
+            file = sys.argv[1]
+        try:
+            with open(file, "r", encoding="utf8") as f:
+                quartz(f.read())
+        except FileNotFoundError:
+            sys.exit(f"Error: File '{sys.argv[1]}' not found")
+        return
+
+    print(f"Quartz {VERSION} ({VERSION_DATE})")
+
+    RUNNING = True
+    while RUNNING:
+        program = input(">>> ")
+        quartz(program)
+
+
+if __name__ == "__main__":
+    main(".\\diceroller.qrtz", True)
