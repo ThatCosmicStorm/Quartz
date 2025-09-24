6bde20cf66e5ebf4e380ceaf2dc3f0727785a816
ThatCosmicStorm <astronomerr2@gmail.com>
2025-09-24 19:33:33 -0400

    Move `enum` and `Token` into its own file

--- lexer.py
+++ lexer.py
@@ -1,75 +1,13 @@
 """*The Quartz lexer.*
 """
 
-from dataclasses import dataclass
-from enum import Enum
+from tokendef import Token, Tag
 
 ##############################
-# TOKEN DEFINITIONS
+# SOME TOKEN DEFINITIONS
 ##############################
 
 DIGITS = "0123456789"
-
-
-class Tag(Enum):
-    EOF = "EOF"
-    NEWLINE = "NEWLINE"
-    STRING = "STRING"
-    INTEGER = "INTEGER"
-    FLOAT = "FLOAT"
-    KEYWORD = "KEYWORD"
-    IDENTIFIER = "IDENTIFIER"
-
-    INDENT = "    "
-
-    EQUAL = "="
-    EQUAL_EQUAL = "=="
-    EQUAL_ARROW = "=>"
-    L_PAREN = "("
-    R_PAREN = ")"
-    L_BRACKET = "["
-    R_BRACKET = "]"
-    L_BRACE = "{"
-    R_BRACE = "}"
-    PIPE = "|"
-    PIPE_EQUAL = "|="
-    SEMICOLON = ";"
-    COLON = ":"
-    COLON_EQUAL = ":="
-    PERIOD = "."
-    PERIOD_PERIOD = ".."
-    PERIOD_PERIOD_EQUAL = "..="
-    PERIOD_PERIOD_PERIOD = "..."
-    COMMA = ","
-    PERCENT = "%"
-    PERCENT_EQUAL = "%="
-    PERCENT_L_BRACE = "%{"
-    ASTERISK = "*"
-    ASTERISK_EQUAL = "*="
-    PLUS = "+"
-    PLUS_EQUAL = "+="
-    MINUS = "-"
-    MINUS_EQUAL = "-="
-    ARROW = "->"
-    L_ANGLE = "<"
-    L_ANGLE_EQUAL = "<="
-    L_ANGLE_L_ANGLE = "<<"
-    L_ANGLE_L_ANGLE_EQUAL = "<<="
-    R_ANGLE = ">"
-    R_ANGLE_EQUAL = ">="
-    R_ANGLE_R_ANGLE = ">>"
-    R_ANGLE_R_ANGLE_EQUAL = ">>="
-    CARET = "^"
-    CARET_EQUAL = "^="
-    TILDE = "~"
-    TILDE_EQUAL = "~="
-    TILDE_ARROW = "~>"
-    SLASH = "/"
-    SLASH_EQUAL = "/="
-    SLASH_SLASH = "//"
-    AMPERSAND = "&"
-    AMPERSAND_EQUAL = "&="
-
 
 KEYWORDS = {
     "if",
@@ -101,13 +39,6 @@ KEYWORDS = {
     "match",
     "case",
 }
-
-
-@dataclass(slots=True, frozen=True)
-class Token:
-    tag: Tag | str
-    tok: str | None
-
 
 ##############################
 # LEXER
