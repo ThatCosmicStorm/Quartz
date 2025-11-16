7656dbeb3fce88ebde2da2c3253188b9e8055a01
ThatCosmicStorm <astronomerr2@gmail.com>
2025-11-16 06:22:33 -0500

    Update quartz.py

--- quartz.py
+++ quartz.py
@@ -4,17 +4,30 @@ Currently only displays the output of the lexer.
 
 import os
 import sys
+from pprint import pprint
+from tokendef import Token
+from nodes import Program
 from lexer import Lexer
+from parser import Parser
+
 os.system("cls" if os.name == "nt" else "clear")
 
 VERSION = "InDev"
-VERSION_DATE = "September 1, 2025"
+VERSION_DATE = """November 16, 2025
+(it's 6:20 AM rn. i didn't wake up, i stayed up.)"""
 
 
 def quartz(program):
-    tokens = Lexer(program).lexer()
-    for token in tokens:
-        print(token)
+    tokens: list[Token] = Lexer(program).lexer()
+    parsed: Program = Parser(tokens).parse()
+    """
+    Okay, we got the parser done.
+    So I'ma just print whatever gets parsed.
+
+    Last stop, implementation!!!
+    (and a bunch of other features here and there)
+    """
+    pprint(parsed)
 
 
 def main(filename=None, debugging=False):
@@ -27,7 +40,9 @@ def main(filename=None, debugging=False):
             with open(file, "r", encoding="utf8") as f:
                 quartz(f.read())
         except FileNotFoundError:
-            sys.exit(f"Error: File '{sys.argv[1]}' not found")
+            sys.exit(
+                f"Error: File '{sys.argv[1]}' not found"
+            )
         return
 
     print(f"Quartz {VERSION} ({VERSION_DATE})")
@@ -39,4 +54,7 @@ def main(filename=None, debugging=False):
 
 
 if __name__ == "__main__":
-    main(".\\diceroller.qrtz", True)
+    main(
+        filename=".\\parsetest.qrtz",
+        debugging=True,
+    )
