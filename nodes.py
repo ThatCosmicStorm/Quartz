e06355bf3574ac30f4e6046aa7b1b52555a721c3
ThatCosmicStorm <astronomerr2@gmail.com>
2026-02-25 18:20:07 -0500

    Update nodes.py
    Each Quartz AST node will get its own `dataclass`.

--- nodes.py
+++ nodes.py
@@ -1,37 +1,62 @@
-"""*Carries `Node` and `BinaryOp`*."""
+"""*Carries all Quartz AST Nodes*."""
 
 ##############################
 # IMPORTS
 ##############################
 
-from typing import NamedTuple
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
-from tokendef import Tag
+if TYPE_CHECKING:
+    from .tokendef import Tag
 
 ##############################
 # NODES
 ##############################
 
 
-class Node(NamedTuple):
+type Expr = Node | BinaryOp
+
+
+@dataclass(frozen=True, slots=True)
+class Node:
     """*All statements and almost all expressions*.
 
-    Operations with two operands have their own Node.
+    Expressions which are binary or unary operations have their own base node.
     """
 
-    name: str
-    data: object
 
-
-class BinaryOp(NamedTuple):
+@dataclass(frozen=True, slots=True)
+class BinaryOp:
     """*Binary operation: two operands*.
 
     Can also store unary operations by setting `left=None`.
     """
 
     op: str | Tag
-    left: object
-    right: object
+    left: Expr | None
+    right: Expr
+
+
+@dataclass(frozen=True, slots=True)
+class Program:
+    """*Contains all statements and expressions for the program*."""
+
+    statements: list[Node | BinaryOp]
+
+
+@dataclass(frozen=True, slots=True)
+class Ident(Node):
+    """*Identifier*."""
+
+    name: str
+
+
+@dataclass(frozen=True, slots=True)
+class Integer(Node):
+    """*Integer*."""
+
+    value: int
 
 
 ##############################
